284da0567b4ade9390412544cc19de2cb5c2921d
Zodik09 <adarshvishwakarma09nov2k@gmail.com>
2024-12-26 18:38:26 +0530

    Created the Homepage and Posts pages, with the post writing functionality.

--- Blog/index.js
+++ Blog/index.js
@@ -1,26 +1,35 @@
 import express from "express";
-import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+let formData = [];
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/",(req, res)=>{
     res.render("index.ejs");
 })
-app.get("/services",(req, res)=>{
-    res.render("services.ejs");
-})
-app.get("/contact",(req, res)=>{
-    res.render("contact.ejs");
-})
+
+app.post("/create", (req, res) => {
+    const {title, desc} = req.body;
+    formData.push({title, desc});
+    res.redirect("/posts");
+});
+
+app.get("/posts", (req, res) => {
+    res.render("posts.ejs", { formData });
+});
 app.get("/about",(req, res)=>{
     res.render("about.ejs");
 })
-app.get("/terms",(req, res)=>{
-    res.render("terms.ejs");
-})
+
+// app.get("/contact",(req, res)=>{
+//     res.render("contact.ejs");
+// })
+// app.get("/terms",(req, res)=>{
+//     res.render("terms.ejs");
+// })
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
